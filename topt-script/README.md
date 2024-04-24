2fdab1483d2967108a5919c80f6e827bfeb3f1ce
王一之 <yz@ggnb.top>
2024-04-24 10:59:25 +0800

    totp动态密码脚本

--- topt-script/README.md
+++ topt-script/README.md
@@ -1,5 +1,9 @@
 # topt 动态密码
 
-脚本猫后台脚本，安装后会在脚本猫菜单页中定时刷新动态密码。
+脚本猫后台脚本，安装后会在脚本猫菜单页中定时刷新动态密码，点击即可复制。
 
 你可以通过 Google Authenticator、Authy 等工具导出二维码作为配置。
+
+![](./assets/img/598cdaa3d47cec69c2cc13a293e2b895.png)
+
+![](./assets/img/image.png)
--- topt-script/src/main.ts
+++ topt-script/src/main.ts
@@ -1,18 +1,64 @@
-import { GM_getValue } from "$";
+import {
+  GM_getValue,
+  GM_registerMenuCommand,
+  GM_setClipboard,
+  GM_unregisterMenuCommand,
+} from "$";
 import { URI } from "otpauth-migration";
+import * as OTPAuth from "otpauth";
 
 const migration = GM_getValue("otpauth.migration") as string;
 
-const totp: string[] = [];
+const totpList: OTPAuth.TOTP[] = [];
 
 migration.split("\n").forEach((line) => {
   if (line.trim().length > 0) {
-    totp.push(...URI.toOTPAuthURIs(line));
+    totpList.push(
+      ...URI.toOTPAuthURIs(line).map((uri) => {
+        const url = new URL(uri);
+        return new OTPAuth.TOTP({
+          issuer: url.searchParams.get("issuer") || url.hostname,
+          algorithm: url.searchParams.get("algorithm") || "SHA1",
+          digits: parseInt(url.searchParams.get("digits") || "6"),
+          period: parseInt(url.searchParams.get("period") || "30"),
+          secret: url.searchParams.get("secret") || "",
+        });
+      })
+    );
   }
 });
 
-console.log(totp);
-
 export default new Promise<void>((resolve) => {
-  resolve();
+  const menuId: string[] = [];
+  // 每分钟的0秒与30秒时刷新
+  const refresh = () => {
+    // 移除上次注册的菜单
+    menuId.forEach((id) => GM_unregisterMenuCommand(id));
+    totpList.forEach((totp) => {
+      console.log(totp.generate());
+      // 注册新的菜单
+      menuId.push(
+        GM_registerMenuCommand(totp.issuer, () => {
+          GM_setClipboard(totp.generate(), "text");
+        })
+      );
+    });
+  };
+  // 计算下一次刷新的时间
+  const next = () => {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const next = seconds < 30 ? 30 : 60;
+    return new Date(now.getTime() + (next - seconds) * 1000);
+  };
+  // 定时器
+  const timer = () => {
+    refresh();
+    setTimeout(timer, next().getTime() - new Date().getTime());
+  };
+  // 启动定时器
+  timer();
+
+  // 屏蔽resolve表示脚本不会自身停止
+  // resolve();
 });
